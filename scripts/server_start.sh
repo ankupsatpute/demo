da8f7489fc20fb243708b73804cdee562fe79468
styagi935 <111732194+styagi935@users.noreply.github.com>
2022-11-17 13:17:14 +0530

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sudo chmod +x /home/ec2-user/tomcat/apache-tomcat-8.5.35/bin/./startup.sh
-sudo chown /home/ec2-user/tomcat/apache-tomcat-8.5.35/bin/
-sudo /home/ec2-user/tomcat/apache-tomcat-8.5.35/bin/./startup.sh
+sudo chmod +x /home/ec2-user/tomcat/apache-tomcat-8.5.35/bin/startup.sh
+sudo sh /home/ec2-user/tomcat/apache-tomcat-8.5.35/bin/startup.sh start
