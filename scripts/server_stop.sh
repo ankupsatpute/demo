2ec23a6fadc4b31f843ca99611247fd1c4ee4931
styagi935 <111732194+styagi935@users.noreply.github.com>
2022-11-17 13:18:24 +0530

    Update server_stop.sh

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo chmod +x /home/ec2-user/tomcat/apache-tomcat-8.5.35/bin/./shutdown.sh
-sudo /home/ec2-user/tomcat/apache-tomcat-8.5.35/bin/./shutdown.sh
+sudo chmod +x /home/ec2-user/tomcat/apache-tomcat-8.5.35/bin/shutdown.sh
+sudo sh /home/ec2-user/tomcat/apache-tomcat-8.5.35/bin/shutdown.sh stop
+ 
