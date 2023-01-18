b2ae48f94be43945b18f0266556544f9e62dd0cb
ankupsatpute <95556178+ankupsatpute@users.noreply.github.com>
2023-01-18 18:22:48 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,5 +1,5 @@
 def deploy(){
        sh """
-         scp -v -o StrictHostkeyChecking=no $WORKSPACE/target/*.war ec2-user@{env.deploy}:/opt/apache-tomcat-9.0.70/webapps
+         scp -v -o StrictHostkeyChecking=no $WORKSPACE/target/*.war ec2-user@172.31.7.56:/opt/apache-tomcat-9.0.70/webapps
          """
     }
