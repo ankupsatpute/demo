6daac8ce4520097aeaa4ba1323cb78193fd34d41
ankupsatpute <95556178+ankupsatpute@users.noreply.github.com>
2023-01-25 16:08:17 +0530

    Update sonarQube.groovy

--- vars/sonarQube.groovy
+++ vars/sonarQube.groovy
@@ -1,6 +1,3 @@
-def sonarQube(String projectKey, String url, String login){
-   sh "mvn sonar:sonar \
-   -Dsonar.projectKey=${projectKey} \
-   -Dsonar.host.url=${url} \
-   -Dsonar.login=${login}"
+def sonarQube(){
+   sh "mvn sonar:sonar"
 }
