f4285342b29f5e384ceb7d915387c21018b98974
ankupsatpute <95556178+ankupsatpute@users.noreply.github.com>
2023-01-25 11:02:14 +0530

    Update fetchCode.groovy

--- vars/fetchCode.groovy
+++ vars/fetchCode.groovy
@@ -1,4 +1,11 @@
-def fetchCode(){
-         git url: "https://github.com/ankupsatpute/demo.git"
+def gitcheckout(){
+          git branch: 'master', changelog: false, poll: false, url: 'https://github.com/ankupsatpute/demo.git'
     }
 
+def gitcheckout1(){
+          git branch: 'develop', changelog: false, poll: false, url: 'https://github.com/ankupsatpute/demo.git'
+}
+
+def gitcheckout2(){
+          git branch: 'feature', changelog: false, poll: false, url: 'https://github.com/ankupsatpute/demo.git'
+}
