0009d60f5efe2258868e6494f759624086d78d23
ankupsatpute <95556178+ankupsatpute@users.noreply.github.com>
2023-02-01 11:49:25 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "allow_tls" {
+  name = "terraform-sg"
+
+  ingress {
+    description = "Allow port 22 - inbound"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow port 8080 - inbound"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
